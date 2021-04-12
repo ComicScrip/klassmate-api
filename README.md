4f6326537a3e20dcaf8192af9934f4546439b7d1
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-04-12 23:05:59 +0200

    server init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Setup
+
+## Install dependencies
+```sh
+yarn
+```
+## Create your environnement variables
+```
+cp .env.sample .env
+```
+
+# Run the app
+
+```sh
+yarn run start:watch
+```
--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Hello world !');
+const app = require('express')();
+const { SERVER_PORT, inTestEnv } = require('./env');
+
+// app settings
+app.set('x-powered-by', false); // for security
+
+// routes
+// ...
+
+// server setup
+app.listen(SERVER_PORT, () => {
+  if (!inTestEnv) {
+    console.log(`Server running on port ${SERVER_PORT}`);
+  }
+});
+
+// process setup : improves error reporting
+process.on('unhandledRejection', (error) => {
+  console.error('unhandledRejection', JSON.stringify(error), error.stack);
+  process.exit(1);
+});
+process.on('uncaughtException', (error) => {
+  console.error('uncaughtException', JSON.stringify(error), error.stack);
+  process.exit(1);
+});
+process.on('beforeExit', () => {
+  app.close((error) => {
+    if (error) console.error(JSON.stringify(error), error.stack);
+  });
+});
