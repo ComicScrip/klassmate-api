7267da4ecf56ca98e362fbbaa0e2362a6b5aea39
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-06-08 15:57:30 +0200

    remove user seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,31 +1,6 @@
 const db = require('../db');
-const User = require('../models/user');
 
 module.exports = async function seed() {
-  const hashedPassword = await User.hashPassword('JS/React0321');
-  await db.user.createMany({
-    data: [
-      { lastName: 'BERDALA', firstName: 'Doriane' },
-      { lastName: 'BOUTRIG', firstName: 'Youcef' },
-      { lastName: 'DUBOIS', firstName: 'Cécile' },
-      { lastName: 'GATTO', firstName: 'Ornella' },
-      { lastName: 'GERARD', firstName: 'Solène' },
-      { lastName: 'JAIMOND', firstName: 'Florian' },
-      { lastName: 'JESUS', firstName: 'Nelson' },
-      { lastName: 'KAMALO', firstName: 'Herança' },
-      { lastName: 'MISSET', firstName: 'Edouard' },
-      { lastName: 'MONGE', firstName: 'Brandon' },
-      { lastName: 'REDONDO', firstName: 'Benoit' },
-      { lastName: 'SCHNUR', firstName: 'Priscilia' },
-      { lastName: 'GABORIT', firstName: 'Jonathan' },
-      { lastName: 'MAUPIED', firstName: 'Joris' },
-    ].map((student) => ({
-      ...student,
-      email: `${student.firstName.toLowerCase()}.${student.lastName.toLowerCase()}@klassmate.app`,
-      hashedPassword,
-    })),
-  });
-
   await db.note.create({
     data: {
       title: 'test1',
