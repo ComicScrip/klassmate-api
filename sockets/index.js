7b40dcf5e46d67918080b3d5b5d0ea91087ca69f
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-06-22 16:29:07 +0200

    Revert "remove lc content"
    This reverts commit 03e20e88832b95e8b63b1ab16de7b2a88ceb865e.

--- sockets/index.js
+++ sockets/index.js
@@ -1,16 +1,34 @@
-// const { CORS_ALLOWED_ORIGINS } = require('../env');
-// const sessions = require('../middlewares/sessions');
-// const User = require('../models/user');
-// const { manageActivityRooms } = require('./activityRooms');
+const socketIO = require('socket.io');
+const { CORS_ALLOWED_ORIGINS } = require('../env');
+const sessions = require('../middlewares/sessions');
+const User = require('../models/user');
+const { manageActivityRooms } = require('./activityRooms');
 
 const initSockets = (server) => {
-  console.log(server);
-  // TODO: instanciate socket server with cors/origin and cors/withCredentials
+  const io = socketIO(server, {
+    cors: {
+      origin: CORS_ALLOWED_ORIGINS.split(','),
+      credentials: true,
+    },
+  });
 
-  // setup middlewares to authenticate every ws request : io.use((socket, next) => sessions(...))
-  // in another middleware : get currentUser from socket.request.session.userId. otherwise disconnect the socket
+  io.use((socket, next) => sessions(socket.request, socket, next));
+  io.use(async (socket, next) => {
+    try {
+      // eslint-disable-next-line
+      socket.request.currentUser = await User.findOne(
+        socket.request.session.userId
+      );
+    } catch (err) {
+      console.error(err);
+      return socket.disconnect();
+    }
+    return next();
+  });
 
-  // handle connect
+  io.on('connect', async (socket) => {
+    manageActivityRooms(socket);
+  });
 };
 
 module.exports = {
